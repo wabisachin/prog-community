f6c5bb0f92509813378628ca444a6330fb6c123a
wabisachin <wabisachin1122@yahoo.co.jp>
2019-10-11 02:13:01 +0000

    コントローラにshowアクションとcreteアクション,viewページの追加

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,4 +3,18 @@ class SchedulesController < ApplicationController
     def new
         @schedule = Schedule.new
     end
+    
+    def create
+        binding.pry
+        Schedule.create(schedule_params)
+        redirect_to schedules_path
+    end
+    
+    def show 
+        @schedule =  Schedule.all
+    end
+    
+    def schedule_params
+        params.require(:schedule).permit(:user_id, :start_at, :finish_at, :location, :comment)
+    end
 end
